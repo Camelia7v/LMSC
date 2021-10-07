e8e6d7c9d003c615a35474aafa953724f0d3cfb5
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-10-07 22:50:26 +0300

    + task 3

--- Tema-Lab2/main.py
+++ Tema-Lab2/main.py
@@ -23,4 +23,53 @@ list(soup.children)
 
 # the tokenizer
 text = open("corpus.txt", "r", encoding="utf-8").read()
-print(re.split(' |\n', text))
+words_list = re.split(r' |\n|\[\d+\]|((?:Dl|Dlui|Dna|Dnei|Dra|Drei|Dr|D-ei|D-lui|D-sa|D-sale|D-ta|Dv|Dvs|D-voastră|dl|dlui|dna|dnei|dra|drei|dr|d-ei|d-lui|d-sa|d-sale|d-ta|dv|dvs|d-voastră|[A-Z]).?\s(?:[A-Z][a-z]+\s*){0,4}|(?:a\.c\.|art\.|cap\.|cca\.|ex\.|etc\.|nr\.|obs\.|op\.cit\.|pag\.|p\.|ș\.a\.|A\.c\.|Art\.|Cap\.|Cca\.|Ex\.|Etc\.|Nr\.|Obs\.|Op\.cit\.|Pag\.|Ş\.a\.))', text)
+# print(words_list)
+
+words_list1 = list()
+for word in words_list:
+    if word != "" and word is not None:
+        words_list1.append(word)
+# print(words_list1)
+
+for i in range(0, len(words_list1)):
+    if '\n' in words_list1[i] or ' ' in words_list1[i]:
+        words_list1[i] = words_list1[i].strip()
+print(words_list1)
+
+patterns = [".", "!", "?", ",", ":", ";", "(", ")", '„', '”', '"']
+cuvinte_abreviate = ["a.c.", "art.", "cap.", "cca.", "ex.", "etc.", "nr.", "obs.", "op.cit.", "pag.", "p.", "ș.a.", "A.c.", "Art.", "Cap.", "Cca.", "Ex.", "Etc.", "Nr.", "Obs.", "Op.cit.", "Pag.", "Ş.a."]
+
+f = open("corpus2.txt", "ab")
+for word in words_list1:
+    if any(x in word for x in patterns):
+        word3 = list(filter(None, re.split('([.!?:;\(\),„”"])+$', word)))
+        word1 = list(filter(None, re.split('^([.!?:;\(\),„”"])+', word3[0])))
+        print(word1[::-1], word, word3[1::])
+        # print(word1, word2, word3)
+
+
+# f = open("corpus1.txt", "ab")
+# for word in words_list1:
+#     string = word + "\n"
+#     if word[0] in patterns:
+#         string = word[0] + "\n" + word[1::] + "\n"
+#     if word[-1] in patterns:
+#         string = word[0:-1] + "\n" + word[-1] + "\n"
+#     if len(word) >= 2 and word[-2] in patterns and word[-1] in patterns:
+#         string = word[0:-2] + "\n" + word[-2] + "\n" + word[-1] + "\n"
+#     if len(word) >= 3 and word[-3] in patterns and word[-2] in patterns and word[-1] in patterns:
+#         string = word[0:-3] + "\n" + word[-3] + "\n" + word[-2] + "\n" + word[-1] + "\n"
+#     if len(word) >= 3 and word[0] in patterns and word[-1] in patterns:
+#         string = word[0] + "\n" + word[1:-1] + "\n" + word[-1] + "\n"
+#     if len(word) >= 3 and word[0] in patterns and word[-2] in patterns and word[-1] in patterns:
+#         string = word[0] + "\n" + word[1:-2] + "\n" + word[-2] + "\n" + word[-1] + "\n"
+#     if len(word) >= 4 and word[0] in patterns and word[-3] in patterns and word[-2] in patterns and word[-1] in patterns:
+#         string = word[0] + "\n" + word[1:-3] + "\n" + word[-3] + "\n" + word[-2] + "\n" + word[-1] + "\n"
+#     if word in cuvinte_abreviate:
+#         string = word + "\n"
+#     f.write(string.encode("UTF-8"))
+
+# '(?:Dl|Dlui|Dna|Dnei|Dra|Drei|Dr|D-ei|D-lui|D-sa|D-sale|D-ta|Dv|Dvs|D-voastră|dl|dlui|dna|dnei|dra|drei|dr|d-ei|d-lui|
+# d-sa|d-sale|d-ta|dv|dvs|d-voastră)\s(?.)(?:[A-Z][a-z]+\s*){1,8}'
+# (etc.))".
