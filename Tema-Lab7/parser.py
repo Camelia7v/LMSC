6ceba879ea3515c36de495d5b1867404b39d7cee
Camelia7v <62173925+Camelia7v@users.noreply.github.com>
2021-11-21 12:40:10 +0200

    wrote the phrase structure trees in a file

--- Tema-Lab7/parser.py
+++ Tema-Lab7/parser.py
@@ -14,12 +14,19 @@ def json_file_parser(file):
 
 if __name__ == "__main__":
     # Parse a json file and extract the texts from it
-    file_name = "Training data/train2.json"
+    file_name = "Training data/train1.json"
     list_of_texts = json_file_parser(file_name)
     # print(len(list_of_texts), list_of_texts)
 
-    # Get the phrase structure trees
-    parser = CoreNLPParser()
+    # Start the server in the background first
+    # Run the server (from the Terminal) using all jars in the CoreNLP home directory ("stanford-corenlp-4.3.1")
+    # with the command below:
+    # java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 50000
+
+    # Get the phrase structure trees and write them in a file
+    parser = CoreNLPParser(url='http://localhost:9000')
+    f = open("phrase_structure_trees.txt", "w")
     for text in list_of_texts:
         parse = next(parser.raw_parse(text))
-        print(parse, "\n\n")
+        f.write(str(parse) + "\n\n\n")
+        # print(parse, "\n\n")
